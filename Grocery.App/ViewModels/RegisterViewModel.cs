70559ceb6af33b2afd0a6b1c339ea2e48e68c36c
Tristangbte <148089122+Tristangbte@users.noreply.github.com>
2025-09-26 15:12:32 +0200

    use case 9 registratie er is een registratie pagina aangemaakt de knop hiervoor staat onder login in het loginscherm

---
+++ Grocery.App/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,60 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+using System.Xml.Linq;
+
+namespace Grocery.App.ViewModels
+{
+    public partial class RegisterViewModel : BaseViewModel
+    {
+        private readonly IAuthService _authService;
+
+        [ObservableProperty]
+        private string name;
+
+        [ObservableProperty]
+        private string email;
+
+        [ObservableProperty]
+        private string password;
+
+        [ObservableProperty]
+        private string registerMessage;
+
+        public RegisterViewModel(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public string RegisterMessage { get; private set; }
+        public string Password { get; private set; }
+        public string Name { get; private set; }
+
+        [RelayCommand]
+        private void Register()
+        {
+            var client = new Client
+            {
+                Name = Name,
+                Email = Email,
+                Password = Password
+            };
+
+            bool success = _authService.Register(client); 
+
+            if (success)
+            {
+                RegisterMessage = "Account succesvol aangemaakt!";
+                
+                Shell.Current.GoToAsync("..");
+            }
+            else
+            {
+                RegisterMessage = "Registratie mislukt. Probeer opnieuw.";
+            }
+        }
+    }
+}
+
